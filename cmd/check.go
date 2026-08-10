653d1ee70589d62487815f4153b650e2eceb04a8
aniball <kakillama@gmail.com>
2026-08-10 19:37:20 -0400

    refactorice: check cmd and check manager (separation of concerns)

--- cmd/check.go
+++ cmd/check.go
@@ -4,6 +4,14 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"apiLunchLite/internal/utils"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 	"github.com/spf13/cobra"
 )
 
@@ -13,7 +21,47 @@ var checkCmd = &cobra.Command{
 	Long:  `Muestra las APIs guardadas y su estado`,
 	Run: func(cmd *cobra.Command, args []string) {
 		apiMgr.CheckLife()
-		apiMgr.Check()
+		result, err := apiMgr.Check()
+
+		if err != nil {
+			log.Fatal("Error: ", err)
+		}
+
+		if len(result) == 0 {
+			fmt.Println("No hay APIs registradas. Usa el comando 'addApi' para empezar.")
+		}
+
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+
+		t.AppendHeader(table.Row{"ID", "Nombre", "Host", "State", "PID"})
+
+		for _, api := range result {
+			displayState := utils.Capitalize(api.State)
+			switch api.State {
+			case "stop":
+				displayState = utils.Stop(displayState)
+			case "run":
+				displayState = utils.Run(displayState)
+			default:
+				displayState = utils.Yellow(displayState)
+			}
+
+			t.AppendRow(table.Row{
+				api.Id,
+				api.Name,
+				fmt.Sprintf("%s:%s", api.Host, utils.Yellow(strconv.Itoa(api.Port))),
+				displayState,
+				api.Pid,
+			})
+		}
+
+		t.SetStyle(table.StyleRounded)
+
+		// Convierte el texto en el header de la tabla en negrita
+		t.Style().Color.Header = text.Colors{text.Bold}
+		t.Render()
+
 	},
 }
 
--- internal/services/manager/check.go
+++ internal/services/manager/check.go
@@ -1,60 +1,16 @@
 package manager
 
 import (
-	"apiLunchLite/internal/utils"
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-
-	"github.com/jedib0t/go-pretty/v6/table"
-	"github.com/jedib0t/go-pretty/v6/text"
+	"apiLunchLite/models"
 )
 
-func (m *ApiManager) Check() error {
-
-	t := table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
+func (m *ApiManager) Check() ([]models.ApiType, error) {
 
 	result, err := m.Db.GetAll()
 
 	if err != nil {
-		log.Fatal("Error:", err)
-		return err
+		return nil, err
 	}
 
-	if len(result) == 0 {
-		fmt.Println("No hay APIs registradas. Usa el comando 'addApi' para empezar.")
-		return nil
-	}
-
-	t.AppendHeader(table.Row{"ID", "Nombre", "Host", "State", "PID"})
-
-	for _, api := range result {
-		displayState := utils.Capitalize(api.State)
-		switch api.State {
-		case "stop":
-			displayState = utils.Stop(displayState)
-		case "run":
-			displayState = utils.Run(displayState)
-		default:
-			displayState = utils.Yellow(displayState)
-		}
-
-		t.AppendRow(table.Row{
-			api.Id,
-			api.Name,
-			fmt.Sprintf("%s:%s", api.Host, utils.Yellow(strconv.Itoa(api.Port))),
-			displayState,
-			api.Pid,
-		})
-	}
-
-	t.SetStyle(table.StyleRounded)
-
-	// Convierte el texto en el header de la tabla en negrita
-	t.Style().Color.Header = text.Colors{text.Bold}
-	t.Render()
-
-	return nil
+	return result, nil
 }
